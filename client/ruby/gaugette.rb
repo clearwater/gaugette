edcb14a1be3bdfa025e171a8bf560f3fb8f7b9b9
Guy Carpenter <guy@clearwater.com.au>
2012-01-23 11:11:42 +1000

    Added new methods for dynamically setting speed on the fly.

--- client/ruby/gaugette.rb
+++ client/ruby/gaugette.rb
@@ -9,6 +9,18 @@ class Gaugette
     @port.write("z %s 0\n" % [address])
   end
 
+  def accel(address, accel, decel)
+    @port.write("a %s %d,%d\n" % [address, accel, decel])
+  end
+
+  def delay(address, min, max)
+    @port.write("d %s %d,%d\n" % [address, min, max])
+  end
+
+  def speed(address, min, max)
+    @port.write("v %s %d,%d\n" % [address, min, max])
+  end
+
   def set(address, fraction)
     @port.write "s %s %d\n" % [address, fraction * @steps]
   end
