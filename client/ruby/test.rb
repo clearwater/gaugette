0e6657eb091b58ae89f746af0e25929a334fb08a
Guy Carpenter <guy@clearwater.com.au>
2012-01-23 16:11:00 +1000

    Add some comments to the test script.

--- client/ruby/test.rb
+++ client/ruby/test.rb
@@ -1,12 +1,6 @@
-# appears we can pull
-# GET /ifstats.php?if=vr0
-# from pfsense
-# after authentication
+# Exercise the gaugette device
 
 require 'rubygems'
-require 'curb'
-require 'sqlite3'
-require 'database'
 require 'serialport'
 require 'gaugette'
 require 'pp'
@@ -15,6 +9,7 @@ require 'pp'
 @gaugette.zero(0)
 @gaugette.zero(1)
 
+# Both motors move in sync
 v = 0
 6.times do |i|
   @gaugette.set(1, v)
@@ -23,10 +18,12 @@ v = 0
   v = 1-v
 end
 
+# Center both motors
 @gaugette.set(0, 0.5)
 @gaugette.set(1, 0.5)
 sleep 2.5
 
+# Motor 1 leads 0 by 0.5 seconds
 6.times do |i|
   @gaugette.set(1, v)
   sleep 0.5
@@ -35,10 +32,12 @@ sleep 2.5
   v = 1-v
 end
 
+# Center both motors
 @gaugette.set(0, 0.5)
 @gaugette.set(1, 0.5)
 sleep 2.5
 
+# Motors in opposite phase
 6.times do |i|
   @gaugette.set(0, 1-v)
   @gaugette.set(1, v)
@@ -46,8 +45,9 @@ sleep 2.5
   v = 1-v
 end
 
+# Center both motors
 @gaugette.set(0, 0.5)
 @gaugette.set(1, 0.5)
-sleep 2.5
+
 
 
